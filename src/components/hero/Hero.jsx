6a485d76c2fcf3067e392698eeb1df76941eff19
benarba mohammed <tewfikhotspur@gmail.com>
2023-10-08 18:25:44 +0200

    add hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <header className="bg-[#F3F3F3] h-screen pt-[6rem]">
+      <div>
+        <div>
+          <Image />
+        </div>
+        <div>
+          <h1>hi,I'm benarba mohammed tewfik</h1>
+          <h3>a passionate frontedn developer from algeria</h3>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar/Navbar";
+// import Navbar from "@/components/navbar/Navbar";
+
+import Hero from "@/components/hero/Hero";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <Hero />
     </main>
   );
 }
